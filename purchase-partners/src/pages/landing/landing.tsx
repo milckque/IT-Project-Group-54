e6a8e1b47c12a5b93e9688f18cba6abd9833b4e9
LamInThisWeirdWorld <lamishere1715@gmail.com>
2025-09-23 20:57:46 +1000

    landing page

--- purchase-partners/src/pages/landing/landing.tsx
+++ purchase-partners/src/pages/landing/landing.tsx
@@ -1,23 +1,34 @@
-
-
 function Landing() {
-    return (
-        <div className="landing-page flex size-full border-1 border-black">
-            <div className="bg-graphic bg-amber-100 border-black p-4 flex-5">
-                <p>Placeholder</p>
-            </div>
-            <div className="interactive-side flex flex-col flex-4 justify-center bg-white">
-                <h1 className="text-4xl m-4 text-yellow-500 font-bold">Welcome to Purchase Partners</h1>
-                <div className="m-2 border-2">
-                    button
-                </div>
-                <div className="m-2 border-2">
-                    button
-                </div>
-                <a href="/dashboard" className="m-2 text-yellow-500 text-center">Continue as Guest</a>
-            </div>
+  return (
+    <div className="landing-page flex size-full border-black">
+      <div className="bg-graphic bg-amber-100">
+        <img src="./bg.png" alt="background" />
+      </div>
+
+      <div className="interactive-side flex flex-col flex-4 justify-center items-center bg-white">
+        <h1 className="font-inter text-7xl m-6 text-yellow-500 font-extrabold text-center">
+          Looking to...
+        </h1>
+
+        <div className="m-2 w-100 h-15 px-6 py-3 bg-black rounded-full shadow-lg hover:shadow-xl cursor-pointer text-2xl text-white text-center font-inter font-bold flex items-center justify-center gap-2">
+          <img src="./Bag_fill.svg" alt="Bag" />
+          SELL
         </div>
-    );
+
+        <div className="m-2 w-100 h-15 px-6 py-3 bg-white border-2 border-[#FFC107] rounded-full shadow-lg hover:shadow-xl cursor-pointer text-black text-2xl text-center font-inter font-bold flex items-center justify-center gap-2">
+          <img src="./Shop.svg" alt="Shop" />
+          BUY
+        </div>
+
+        <a
+          href="/dashboard"
+          className="m-4 text-yellow-500 text-center font-poppins"
+        >
+          Continue as Guest
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
