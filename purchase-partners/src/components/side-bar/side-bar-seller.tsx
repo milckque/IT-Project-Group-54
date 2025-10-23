f60fd94c1e03b4fe44366d522f25873f3cfb6ed6
gheu <gladys.heu@student.unimelb.edu.au>
2025-10-23 21:23:08 +1100

    fixed seller side bar

--- purchase-partners/src/components/side-bar/side-bar-seller.tsx
+++ purchase-partners/src/components/side-bar/side-bar-seller.tsx
@@ -2,6 +2,7 @@ import SideBarItem from "./side-bar-item";
 import { forwardRef } from "react";
 import "./side-bar.css";
 import { useAuth } from "../../backend/AuthContext";
+import { useProfile } from "../../hooks/useProfile";
 
 // css to handle the sliding in and out of the sidebar
 const width = 20; // Width in rem
@@ -14,13 +15,16 @@ const sidebarStyles = (isOpen: boolean) => ({
 const SideBarSeller = forwardRef<HTMLDivElement, { isOpen?: boolean }>(
   ({ isOpen = false }, ref) => {
     const { signOut, loading, user } = useAuth();
+    const { profile } = useProfile();
     return (
       <div
         ref={ref}
         style={sidebarStyles(isOpen)}
         className={`side-bar bg-yellow-200 h-full flex flex-col space-y-4 p-4 border-b border-gray-300 absolute top-0 z-10 overflow-hidden`}
       >
-        {/* <div className="header">Insert stuff here</div> */}
+        <div className="header w-full flex items-center justify-center py-3 text-3xl ">
+          Hello, {profile?.first_name ?? "there"}!
+        </div>
 
         <div className="body flex flex-col space-y-2">
           <SideBarItem title="Account" href="/seller-dashboard" />
